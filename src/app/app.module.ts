e297decaf89276b4666b0bba0f1668af0083dec0
mstiven013 <32859798+mstiven013@users.noreply.github.com>
2018-07-23 22:28:59 -0500

    Header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/common/header/top-bar/top-bar.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { NavMenuComponent } from './components/common/header/nav-menu/nav-menu.component';
+import { SocialNetworkComponent } from './components/common/social-network/social-network.component';
+import { LogoComponent } from './components/common/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    HeaderComponent,
+    NavMenuComponent,
+    SocialNetworkComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/common/social-network/social-network.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'social-network',
+  templateUrl: './social-network.component.html',
+  styleUrls: ['./social-network.component.css']
+})
+export class SocialNetworkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
