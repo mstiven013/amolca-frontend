8688626a22cf682da70bca06d4629eba1ba069b8
Stiven Lopez <32859798+mstiven013@users.noreply.github.com>
2018-08-15 16:17:33 -0500

    Authors Carousel component + Books Carousel text align center

--- src/app/components/authors/authors-carousel/authors-carousel.component.ts
+++ src/app/components/authors/authors-carousel/authors-carousel.component.ts
@@ -1,20 +1,61 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { TooltipPosition } from '@angular/material';
 import { NgxCarousel } from 'ngx-carousel';
+import { GetAuthorService } from '../../../services/author/get-author.service';
 
 declare var jQuery: any;
 declare var Materialize: any;
 
 @Component({
-  selector: 'app-authors-carousel',
+  selector: 'authors-carousel',
   templateUrl: './authors-carousel.component.html',
   styleUrls: ['./authors-carousel.component.scss']
 })
 export class AuthorsCarouselComponent implements OnInit {
 
-  constructor() { }
+  //Ngx Carousel vars
+  authorsCarousel: NgxCarousel;
+  @Input() itemsPerRow: any = 4;
+
+  //Custom vars
+  authors: any = [];
+
+  constructor(
+    private _getAuthorService: GetAuthorService
+  ) { }
 
   ngOnInit() {
+    this.getAuthorsInfo();
+    this.generateCarousel();
+  }
+
+  getAuthorsInfo() {
+    this._getAuthorService.getAllAuthors()
+      .map(resp => resp.json())
+      .subscribe(
+        data => { this.authors = data },
+        err => { console.log(err.json()) }
+      )
+  }
+
+  generateCarousel() {
+    let me = this;
+    this.authorsCarousel = {
+      grid: {xs: 1, sm: 2, md: 2, lg: me.itemsPerRow, all: 0},
+      slide: 1,
+      speed: 400,
+      interval: 4000,
+      point: {
+        visible: true
+      },
+      load: 2,
+      touch: true,
+      loop: true,
+      custom: 'banner'
+    }
+  }
+
+  myfunc(event: Event) {
+    //
   }
 
 }
