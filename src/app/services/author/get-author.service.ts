adcf5a5b0c069faccdea18229c2fa5a95d0e8dc2
mstiven013 <32859798+mstiven013@users.noreply.github.com>
2018-10-08 00:04:21 -0500

    Fix get All Authors service query

--- src/app/services/author/get-author.service.ts
+++ src/app/services/author/get-author.service.ts
@@ -12,11 +12,13 @@ export class GetAuthorService {
     private _http: Http
   ) {  }
 
-  getAllAuthors() {
+  getAllAuthors({orderby='title', order='asc',limit=2}: {orderby?: string, order?: string, limit?: number}={}) {
     let headers = new Headers({'Content-type': 'application/json'});
     let options = new RequestOptions({headers: headers});
 
-    return this._http.get(config.API_URL + '/authors', options);
+    let params = `orderby=${orderby}&order=${order}&limit=${limit}`;
+
+    return this._http.get(config.API_URL + '/authors?' + params, options);
   }
 
   getAuthorsById(id) {
