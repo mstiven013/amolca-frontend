46503e23d1900f6db44fcf58c6215db27db543b5
mstiven013 <32859798+mstiven013@users.noreply.github.com>
2018-12-03 10:46:00 -0500

    Arreglar carga de libros en el loop y los carruseles

--- src/app/components/books/books-loop/books-loop.component.ts
+++ src/app/components/books/books-loop/books-loop.component.ts
@@ -72,8 +72,9 @@ export class BooksLoopComponent extends BooksGlobalLoopComponent {
         .map(resp => resp.json())
         .subscribe(
           data => {
-            if(data.length > 0) {
-              this.setBooksInfo(data)
+            if(data.books.length > 0) {
+              this.setBooksInfo(data.books)
+              this.totalBooks = data.count;
             } else {
               let err = { status: 404 }
               this.mapErrors(err, 'especialidad')
