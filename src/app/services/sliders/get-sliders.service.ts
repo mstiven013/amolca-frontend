a3d257811918ad7195e8b1ddc07d777a3b66bfe6
mstiven013 <32859798+mstiven013@users.noreply.github.com>
2018-12-11 10:16:00 -0500

    Agregar servicio de sliders y carga dinamica del mismo

---
+++ src/app/services/sliders/get-sliders.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { config } from '../../../config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetSlidersService {
+
+  constructor(
+    private _http: Http
+  ) {  }
+
+  getAllSliders() {
+    let headers = new Headers({'Content-type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
+     return this._http.get(`${config.API_URL}/sliders/`, options);
+  }
+}
