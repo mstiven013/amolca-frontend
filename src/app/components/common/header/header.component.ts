c236209e75d35cab7b54ad76b6fb82695d255046
mstiven013 <32859798+mstiven013@users.noreply.github.com>
2018-12-21 17:50:55 -0500

    Cambiar redirección para un componente común

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -13,7 +13,11 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.getGeolocalization();
+    localStorage.removeItem('C0uN7r1');
+    let me = this;
+    setTimeout(function() {
+      me.getGeolocalization();
+    }, 200);
   }
 
   getGeolocalization() {
@@ -22,6 +26,23 @@ export class HeaderComponent implements OnInit {
     if(c === null || c === undefined) {
       jQuery.getJSON('http://ip-api.com/json?callback', function(data) {
         localStorage.setItem('C0uN7r1', data.country.toUpperCase());
+
+        c = localStorage.getItem('C0uN7r1');
+
+        if(c !== null) {
+          //Redirecciones REPUBLICA DOMINICANA
+          if(c === 'ARGENTINA' && window.location.href.indexOf("amolca.com.pe") > 0) {
+            window.location.href = 'http://www.amolca.com.ar';
+          } else if(c === 'PERU' && window.location.href.indexOf("amolca.com.pe") > 0) {
+            window.location.href = 'http://www.amolca.com.pe';
+          } else if(c === 'COLOMBIA' && window.location.href.indexOf("amolca.com.pe") > 0) {
+            window.location.href = 'http://www.amolca.com.co';
+          } else if(c === 'DOMINICAN REPUBLIC' && window.location.href.indexOf("amolca.com.do") > 0) {
+            return true;
+          } else {
+            window.location.href = 'http://www.amolca.com';
+          }
+        }
       });
     }
   }
