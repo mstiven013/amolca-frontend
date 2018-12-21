6f3076b48a758adfdea7795ddbf7b53836ad9f4e
mstiven013 <32859798+mstiven013@users.noreply.github.com>
2018-12-21 18:30:43 -0500

    Configuración para enviar información personalizada al crear usuario y crear pedidos

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,11 @@ export const config = {
     //API_URL: 'http://localhost:3000/api/1.0',
     API_URL: 'https://amolca-backend.herokuapp.com/api/1.0',
     client_id: 'myclientid',
-    secret: 'mysecretclient'
+    secret: 'mysecretclient',
+    email: {
+        from: 'Amolca República Dominicana <ventas@amolca.com.do>',
+        cc: 'ventas@amolca.com.do, asistentepresidencia@amolca.us',
+        subject: 'Registro de nuevo pedido - Amolca República Dominicana',
+        domain: 'www.amolca.com.do'
+    }
 }
