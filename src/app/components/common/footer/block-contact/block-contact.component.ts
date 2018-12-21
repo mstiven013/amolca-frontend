13a1eaad3245dd9428e712a97e53e81b5007d08d
mstiven013 <32859798+mstiven013@users.noreply.github.com>
2018-12-21 15:52:48 -0500

    Todos los cambios del pie de página
    - Reducir tamaño
    - Agregar interactividad a los links

--- src/app/components/common/footer/block-contact/block-contact.component.ts
+++ src/app/components/common/footer/block-contact/block-contact.component.ts
@@ -14,10 +14,10 @@ export class BlockContactComponent implements OnInit {
 
   //Test info vars
   contact = [
-    {name: 'direction', data: 'Carrera 43 # 9 sur 195, Edificio Square,<br/> Torre Inexmoda. Medellín, Colombia', state: true},
-    {name: 'phone', data: '', state: false},
-    {name: 'mobile', data: '(574) 4797431', state: true},
-    {name: 'email', data: 'contacto@amolca.com', state: true}
+    {name: 'direction', link: 'https://goo.gl/maps/TQntK7Afu7s', target: '_blank', data: 'Carrera 43 # 9 sur 195, Edificio Square<br/> Torre Inexmoda. Medellín, Colombia', state: true},
+    {name: 'phone', link: '', target: '_self', data: '', state: false},
+    {name: 'mobile', link: 'tel:5744797431', target: '_self', data: '(574) 4797431', state: true},
+    {name: 'email', link: 'mailto:contacto@amolca.com', target: '_self', data: 'contacto@amolca.com', state: true}
   ]
 
 }
