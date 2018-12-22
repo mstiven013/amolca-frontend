1098bf4c0421b99b47f6548c83614c9a3bd8beda
mstiven013 <32859798+mstiven013@users.noreply.github.com>
2018-12-21 22:41:59 -0500

    Bajar tamaño al footer y linkear todos los items del mismo

--- src/app/components/common/footer/block-social/block-social.component.ts
+++ src/app/components/common/footer/block-social/block-social.component.ts
@@ -17,10 +17,10 @@ export class BlockSocialComponent implements OnInit {
   
   //Test info vars
   contact = [
-    {name: 'direction', data: 'Junín 827- Ciudad Autónoma de Buenos Aires', state: true},
-    {name: 'phone', data: '', state: false},
-    {name: 'mobile', data: '(54011) 4961-6506', state: true},
-    {name: 'email', data: 'ventas@amolca.com.ar', state: true}
+    {name: 'direction', link: 'https://goo.gl/maps/fAhBrTPou122', target: '_blank', data: 'Junín 827- Ciudad Autónoma de Buenos Aires', state: true},
+    {name: 'phone', link: '', target: '_self', data: '', state: false},
+    {name: 'mobile', link: 'tel:5401149616506', target: '_self', data: '(54011) 4961-6506', state: true},
+    {name: 'email', link: 'mailto:ventas@amolca.com.ar', target: '_self', data: 'ventas@amolca.com.ar', state: true}
   ]
 
 }
