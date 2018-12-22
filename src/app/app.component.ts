de3e17c92407135ba7b3ee568a39955d29ea7c6d
mstiven013 <32859798+mstiven013@users.noreply.github.com>
2018-12-22 00:23:18 -0500

    Agregar redirecciones en el Cabezote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,32 +14,6 @@ export class AppComponent {
   ) {}
 
   ngOnInit() {
-    localStorage.removeItem('C0uN7r1');
-    let me = this;
-    setTimeout(function() {
-      me.getGeolocalization();
-    }, 200);
-  }
-
-  getGeolocalization() {
-    let c = localStorage.getItem('C0uN7r1');
-
-    if(c === null || c === undefined) {
-      jQuery.getJSON('http://ip-api.com/json?callback', function(data) {
-        localStorage.setItem('C0uN7r1', data.country.toUpperCase());
-      });
-    }
-    
-    if(c !== null) {
-      if(c === 'COLOMBIA' && window.location.href.indexOf("amolca.com.co") < 1) {
-        window.location.href = 'http://www.amolca.com.co';
-      }
-
-      if(c != 'COLOMBIA' && window.location.href.indexOf("amolca.com.co") > 0) {
-        window.location.href = 'http://www.amolca.com';
-      }
-    }
-    
   }
 
   public setMetaTitle( newTitle: string ) {
