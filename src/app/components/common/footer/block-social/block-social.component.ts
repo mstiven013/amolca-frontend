87274f0dbfbe790a7e6b776a3ad4897fc8732dbf
mstiven013 <32859798+mstiven013@users.noreply.github.com>
2018-12-21 19:50:31 -0500

    Bajar tamaño al footer y linkear todos los items del mismo

--- src/app/components/common/footer/block-social/block-social.component.ts
+++ src/app/components/common/footer/block-social/block-social.component.ts
@@ -17,10 +17,10 @@ export class BlockSocialComponent implements OnInit {
   
   //Test info vars
   contact = [
-    {name: 'direction', data: 'Unicentro Plaza 36 Av. Abrham Lincoln y 27 de Febrero, Santo Domingo, República Dominicana', state: true},
-    {name: 'phone', data: '', state: false},
-    {name: 'mobile', data: '(1809) 5658732 – 3811043', state: true},
-    {name: 'email', data: 'ventas@amolca.com.do', state: true}
+    {name: 'direction', link: 'https://goo.gl/maps/vvVMgyNHwQP2', target: '_blank', data: 'Unicentro Plaza 36 Av. Abrham Lincoln y 27 de Febrero, Santo Domingo, República Dominicana', state: true},
+    {name: 'phone', link: '', target: '_self', data: '', state: false},
+    {name: 'mobile', link: 'tel:5658732', target: '_self',  data: '(1809) 5658732 – 3811043', state: true},
+    {name: 'email', link: 'mailto:ventas@amolca.com.do', target: '_self', data: 'ventas@amolca.com.do', state: true}
   ]
 
 }
