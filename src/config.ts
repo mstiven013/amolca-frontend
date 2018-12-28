ced3cc79c325a6067c31cb665c8a5fcad0765aa2
mstiven013 <32859798+mstiven013@users.noreply.github.com>
2018-12-28 15:28:05 -0500

    Cambio URL de API provisional

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 export const config = {
     SERVER: 'server',
+    API_URL: 'https://amolca-api-nodejs.herokuapp.com/api/1.0',
     //API_URL: 'http://localhost:3000/api/1.0',
-    API_URL: 'https://amolca-backend.herokuapp.com/api/1.0',
+    //API_URL: 'https://amolca-backend.herokuapp.com/api/1.0',
     client_id: 'myclientid',
     secret: 'mysecretclient',
     email: {
