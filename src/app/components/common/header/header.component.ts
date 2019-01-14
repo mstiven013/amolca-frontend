bc13fe1304e86fd8953083282e5780cc65d3482d
Stiven Lopez <32859798+mstiven013@users.noreply.github.com>
2019-01-14 16:47:08 -0500

    Cambiar API de Geolocalización

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -21,25 +21,32 @@ export class HeaderComponent implements OnInit {
   }
 
   getGeolocalization() {
-    jQuery.getJSON('http://ip-api.com/json?callback', function(data) {
-      localStorage.setItem('C0uN7r1', data.country.toUpperCase());
-      let c = localStorage.getItem('C0uN7r1');
-      
-      if(c !== null) {
-        //Redirecciones COLOMBIA
-        if(c === 'ARGENTINA' && window.location.href.indexOf("amolca.com.co") > 0) {
-          window.location.href = 'http://www.amolca.com.ar';
-        } else if(c === 'PERU' && window.location.href.indexOf("amolca.com.co") > 0) {
-          window.location.href = 'http://www.amolca.com.pe';
-        } else if(c === 'DOMINICAN REPUBLIC' && window.location.href.indexOf("amolca.com.co") > 0) {
-          window.location.href = 'http://www.amolca.com.do';
-        } else if(c === 'COLOMBIA' && window.location.href.indexOf("amolca.com.co") > 0) {
-          return true;
-        } else {
-          window.location.href = 'http://www.amolca.com';
-        }
-      }
-    });
+    let c = localStorage.getItem('C0uN7r1');
+
+    $.get("https://ipinfo.io", function(response) {
+      //console.log(response);
+
+      let search = country.filter(c => c.code == response.country);
+      let active = search[0].name.toUpperCase();
+
+      localStorage.setItem('C0uN7r1', active.toUpperCase());
+
+      c = localStorage.getItem('C0uN7r1')
+    
+      //Redirecciones COLOMBIA
+      if(c === 'ARGENTINA' && window.location.href.indexOf("amolca.com.co") > 0) {
+        window.location.href = 'http://www.amolca.com.ar';
+      } else if(c === 'PERU' && window.location.href.indexOf("amolca.com.co") > 0) {
+        window.location.href = 'http://www.amolca.com.pe';
+      } else if(c === 'DOMINICAN REPUBLIC' && window.location.href.indexOf("amolca.com.co") > 0) {
+        window.location.href = 'http://www.amolca.com.do';
+      } else if(c === 'COLOMBIA' && window.location.href.indexOf("amolca.com.co") > 0) {
+        return true;
+      } else {
+        window.location.href = 'http://www.amolca.com';
+      } 
+    }, "jsonp")
+
   }
 
 }
